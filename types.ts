7e30525f7797e3dd8818dfab82d547278e0a00a4
wefun74 <wefun74@gmail.com>
2025-06-03 16:23:48 +0900

    💄롤백

--- types.ts
+++ types.ts
@@ -1,4 +1,7 @@
-export interface ScheduleObj {
+// 파일 위치: /school/types.ts
+
+// Calendar schedule item
+export type ScheduleObj = {
   발주처: string;
   낙찰기업: string;
   날짜: string;
@@ -6,10 +9,10 @@ export interface ScheduleObj {
   수량: number;
   계약단가: number;
   공급가액: number;
-}
+};
 
-export interface DocData {
-  연월: string;
+// Firestore data for school document
+export type DocData = {
   발주처: string;
   사업자등록번호: string;
   사업장주소: string;
@@ -19,13 +22,14 @@ export interface DocData {
     식품명: string;
     납품: Record<string, { 수량: number; 계약단가: number; 공급가액: number }>;
   }>;
-}
+};
 
-export interface VendorData {
+// Firestore data for vendor document
+export type VendorData = {
   상호명: string;
   대표자: string;
   대표전화번호: string;
   사업자번호?: string;
   사업자등록번호?: string;
   주소: string;
-}
+};
