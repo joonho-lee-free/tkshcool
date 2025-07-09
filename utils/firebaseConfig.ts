d5f92dbb8759187dacd52dfd5d336262f40be523
wefun74 <wefun74@gmail.com>
2025-07-09 13:18:01 +0900

    index4 평균수정

--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -1,18 +1,18 @@
-// utils/firebaseConfig.ts
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgo4epYkdiQZcYxapDtgI8hM_qLZg68r",
+  apiKey: "AIzaSyBgo9r4epKHiq2CrXpayDtgHB4M_lgzC68",
   authDomain: "tkhealth-30380.firebaseapp.com",
   projectId: "tkhealth-30380",
-  storageBucket: "tkhealth-30380.appspot.com",
-  messagingSenderId: "692960061905",
-  appId: "1:692960061905:web:babfe553aef6aa4dddfd02",
+  storageBucket: "tkhealth-30380.appspot.com", // ✅ 수정
+  messagingSenderId: "692900691095",
+  appId: "1:692900691095:web:babfe553aef6aa4dddfdd2",
+  measurementId: "G-0MYSSGWH84",
 };
 
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export { app };
-export default db;
+
